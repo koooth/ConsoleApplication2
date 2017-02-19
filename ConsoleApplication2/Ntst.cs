9c609076d21952e0b50ae9bfc151fc815df3cb37
vv <vvv>
2017-02-20 00:34:28 +0300

    TCS nullresult + copyright

--- ConsoleApplication2/Ntst.cs
+++ ConsoleApplication2/Ntst.cs
@@ -1,17 +1,48 @@
 ﻿using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Firefox;
 using NUnit.Framework;
 using System.Diagnostics;
+using System.Collections.Generic;
+using System.IO;
+using OpenQA.Selenium.Support.UI;
+//using Excel = Microsoft.Office.Interop.Excel;
+
 
 namespace ConsoleApplication2
 {
     class Ntst
-   { 
+   {
+        public static List<TestCaseData> TS
+        {
+            get
+            {
+                var testCases = new List<TestCaseData>();
+
+                using (var fs = File.OpenRead(@"C:\tst.csv"))
+                using (var sr = new StreamReader(fs))
+                {
+                    string line = string.Empty;
+                    while (line != null)
+                    {
+                        line = sr.ReadLine();
+                        if (line != null)
+                        {
+                            string[] split = line.Split(new char[] { ',' },
+                                StringSplitOptions.None);
+
+                            string jrn = Convert.ToString(split[0]);
+
+                            var testCase = new TestCaseData(jrn);
+                            testCases.Add(testCase);
+                        }
+                    }
+                }
+
+                return testCases;
+            }
+        }
+
         IWebDriver driver;
 
         [SetUp]
@@ -20,19 +51,66 @@ namespace ConsoleApplication2
             driver = new FirefoxDriver();
         }
 
-        [Test]
-        public void JBJS()
+        //[Test]
+        //public void JBJS([Values("prsgo", "jbjsoa", "plasreconsurg")] string journal)
+        //{
+        //    driver.Url = "http://journals.lww.com/" + journal + "/pages/results.aspx?txtkeywords=ggg";
+        //    //Debug.WriteLine("--------------------1");
+        //    driver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(50));
+        //    IWebElement result0 = driver.FindElement(By.CssSelector(".resultCount"));
+        //    //Debug.WriteLine("----------------------2");
+        //    Assert.AreEqual("0 results", result0.Text);
+        //    //Debug.WriteLine("!!!!!!!!!!!!!!!!!!!" + result0.Text);
+        //    //Debug.WriteLine("2nd" + result0.Text);
+        //    //Debug.WriteLine("3rd" + result0.Text);
+        //    Trace.WriteLine(journal + " - " + result0.Text);
+        //    TestContext.WriteLine(journal + " - " + result0.Text);
+        //}
+
+        [TestCaseSource("TS")]
+        public void nullsearch(string jrn)
         {
-            driver.Url = "http://stage-journals.lww.com/jbjsoa/pages/results.aspx?txtkeywords=blood";
-            System.Diagnostics.Debug.WriteLine("--------------------1");
+            driver.Url = "http://journals.lww.com/" + jrn + "/pages/results.aspx?txtkeywords=gggasdqwe";
+            //Debug.WriteLine("--------------------1");
             driver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(50));
             IWebElement result0 = driver.FindElement(By.CssSelector(".resultCount"));
-            System.Diagnostics.Debug.WriteLine("----------------------2");
-            Assert.AreEqual("0 results", result0.Text);
-            System.Diagnostics.Debug.WriteLine("!!!!!!!!!!!!!!!!!!!" + result0.Text);
-            System.Diagnostics.Debug.WriteLine("2nd" + result0.Text);
-            System.Diagnostics.Debug.WriteLine("3rd" + result0.Text);
+            IWebElement copyright = driver.FindElement(By.CssSelector(".copy"));
+            //IWebElement result0 = new WebDriverWait(driver, TimeSpan.FromSeconds(20)).Until(ExpectedConditions.((By.CssSelector(".resultCount"))));
+            //IWebElement copyright = new WebDriverWait(driver, TimeSpan.FromSeconds(20)).Until(ExpectedConditions.((By.CssSelector(".copy")));
+            //Debug.WriteLine("----------------------2");
+            Assert.AreEqual("0 results", result0.Text + " - " + copyright.Text);
+            //Debug.WriteLine("!!!!!!!!!!!!!!!!!!!" + result0.Text);
+            //Debug.WriteLine("2nd" + result0.Text);
+            Debug.WriteLine(jrn + " - " + result0.Text + " - " + copyright.Text);
+            Trace.WriteLine(jrn + " - " + result0.Text + " - " + copyright.Text);
+            TestContext.WriteLine(jrn + " - " + result0.Text + " - " + copyright.Text);
         }
+
+        //public static class WebDriverExtensions
+        //{
+        //    public static IWebElement FindElement(this IWebDriver driver, By by, int timeoutInSeconds)
+        //    {
+        //        if (timeoutInSeconds > 0)
+        //        {
+        //            var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(timeoutInSeconds));
+        //            return wait.Until(drv => drv.FindElement(by));
+        //        }
+        //        return driver.FindElement(by);
+        //    }
+        //}
+
+        //[TestCaseSource("TS")]
+        //public void copyright(string jrn)
+        //{
+        //    driver.Url = "http://journals.lww.com/" + jrn;
+        //    //driver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(50));
+        //    IWebElement copyright = new WebDriverWait(driver, TimeSpan.FromSeconds(20)).Until(ExpectedConditions.((By.CssSelector(".copy"))));
+        //    //driver.FindElement(By.CssSelector(".copy"));
+        //    Assert.IsNotEmpty(copyright.Text);
+        //    Trace.WriteLine(jrn + " - " + copyright.Text);
+        //    TestContext.WriteLine(jrn + " - " + copyright.Text);
+        //    Debug.WriteLine(jrn + " - " + copyright.Text);
+        //}
 
         [TearDown]
         public void Clear()
--- ConsoleApplication2/Program.cs
+++ ConsoleApplication2/Program.cs
@@ -1,16 +1,47 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Excel = Microsoft.Office.Interop.Excel;
 
 
 namespace ConsoleApplication2
 {
     class Program
     {
+        //private string jrn = null;
         static void Main(string[] args)
         {
+            //string url = "http://stage-journals.lww.com/";
+            //string jrn = null;
+            //List <string> rowValue = new List<string> { };
+            //var ExcelFilePath = "C:\\tst.xlsx";
+
+            //Excel.Application xlApp = new Excel.Application(ExcelFilePath);
+            //Excel.Workbook xlWorkbook = xlApp.Workbooks.Open(ExcelFilePath);
+            //Excel._Worksheet xlWorksheet = xlWorkbook.Sheets[1];
+            //Excel.Range xlRange = xlWorksheet.UsedRange;
+
+            //int rowCount = xlRange.Rows.Count;
+            //int colCount = xlRange.Columns.Count;
+
+            //for (int i = 1; i <= rowCount; i++)
+            //{
+            //    for (int j = 1; j <= colCount; j++)
+            //    {
+            //        rowValue.Add(xlRange.Cells[i, j].Value2.ToString());
+
+            //    }
+            //    jrn = url + rowValue[0];
+            //    Console.WriteLine(jrn);
+            //    //return jrn;
+
+            //    rowValue.Clear();
+            //}
+            //Console.Read();
         }
+
+        //public Program(string jrn)
+        //{
+        //    this.jrn = jrn;
+        //}
     }
 }
